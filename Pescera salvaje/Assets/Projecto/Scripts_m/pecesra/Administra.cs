7c6fcb600fb3b6435e0dabee91c2fe9586eff536
Diaz <alejandroivanleyvadiaz@gmail.com>
2020-01-27 18:29:14 -0600

    Fixes
    Google VR and Unity XR tools are not compatible

--- Pescera salvaje/Assets/Projecto/Scripts_m/pecesra/Administra.cs
+++ Pescera salvaje/Assets/Projecto/Scripts_m/pecesra/Administra.cs
@@ -27,19 +27,16 @@ public class Administra : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       /* localizacion_Player = GameObject.FindGameObjectWithTag("Player").transform.position;
-        if (localizacion_Player.y > maxY)
-        {
-            ProcesPlayer.profile = arriba;
-        }
-        else if (ProcesPlayer.profile != abajo) {
-            ProcesPlayer.profile = abajo;
-        }*/
-       if (Input.GetButtonDown("A")) {
-            SceneManager.LoadScene("nv2");
-        } else if (Input.GetButtonDown("B"))
-        {
-            playerson.transform.position = new Vector3(0, -10, 0);
+        /* localizacion_Player = GameObject.FindGameObjectWithTag("Player").transform.position;
+         if (localizacion_Player.y > maxY)
+         {
+             ProcesPlayer.profile = arriba;
+         }
+         else if (ProcesPlayer.profile != abajo) {
+             ProcesPlayer.profile = abajo;
+         }*/
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
         }
     }
 }
